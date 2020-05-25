8642b078ecd809343c7168029da0ab7b182aade4
tejsubedi@gmail.com <tejsubedi@gmail.com>
2020-05-24 21:46:13 -0400

    added card on notifications

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Notifications = () => {
     return (
-        <div>
-            <p>Notifications will be update very soon....</p>
+        <div className="container">
+            <div className="card  blue-grey darken-1">
+                <div className="card-content white-text">
+                    <span className="card-title">Notifications</span>
+                    <div className="card-action grey darken-1">
+                        <p>Notification by </p>
+                        <p>29th April</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
